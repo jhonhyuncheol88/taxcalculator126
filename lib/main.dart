d74e6c71177c422de5731e76a66d0994dc731030
jhonhyuncheol88 <jhc0902@naver.com>
2022-12-11 23:02:12 +0900

    12월 11일 오후 11시

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:intl/intl.dart';
 import 'package:pattern_formatter/pattern_formatter.dart';
 import 'package:flutter/services.dart'; //앱 상단의 stauts 바를 없앨 수 잇음
 
-
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -33,17 +31,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
 // 뉴모피즘 변수 값
-  final  off1 =-2.0;
-  final  off2  =2.0;
-  final  blurR = 2.0;
-  final  spredR = 2.0;
+  final off1 = -2.0;
+  final off2 = 2.0;
+  final blurR = 2.0;
+  final spredR = 2.0;
 
 //
 
- bool cred =false; //신용카드 공제 조건식
+  bool cred = false; //신용카드 공제 조건식
 
   bool click = false; //계산 버튼 애니메이션 변수
 
@@ -57,8 +53,10 @@ class _HomePageState extends State<HomePage> {
   double taxspending = 0; // 지출 과세표준
   double creditcard_help = 0; // 신용카드공제
   double spending_Food_help = 0; //의제 매입 공제
-  double estimate_tax = 0; //예상 부가세  = (t1 *1/10) - (t3 *1/10) - (t1 *13/1000) - (t2*9/109)
+  double estimate_tax =
+      0; //예상 부가세  = (t1 *1/10) - (t3 *1/10) - (t1 *13/1000) - (t2*9/109)
   double overcreditcard_help = 10000000;
+
 //컨트롤러 초기화
   @override
   void initState() {
@@ -80,9 +78,6 @@ class _HomePageState extends State<HomePage> {
   //2. 콤마를 제거한 숫자 값을 double.parse로 변환해준다.
   //3. double로 된 숫자 값을 해당 변수에 넣어주고 계산식을 적용한다.
   void convertStringtoDouble() {
-
-
-
     var t1text = t1.text;
     var t1convert = t1text.replaceAll(",", "");
     var income = double.parse(t1convert);
@@ -95,41 +90,30 @@ class _HomePageState extends State<HomePage> {
     var t3convert = t3text.replaceAll(",", "");
     var spending_Product = double.parse(t3convert);
 
-
-
     taxincome = income * 1 / 10;
     spending_Food_help = spending_Food * 9 / 109;
     taxspending = spending_Product * 1 / 10;
-    creditcard_help =income * 13/1000;
+    creditcard_help = income * 13 / 1000;
 
     //신용카드공제, 의제매입공제 조건식
-      if(creditcard_help >10000000 ) {
-        creditcard_help =10000000;
+    if (creditcard_help > 10000000) {
+      creditcard_help = 10000000;
+    }
 
-      }
-
-    estimate_tax = taxincome-taxspending -creditcard_help-spending_Food_help;
-
-
-
-
-
+    estimate_tax =
+        taxincome - taxspending - creditcard_help - spending_Food_help;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body:
-
-      GestureDetector(
-        onTap: (){
+      body: GestureDetector(
+        onTap: () {
           setState(() {
             FocusScope.of(context).unfocus();
-
           });
         },
-
         child: SafeArea(
             child: SingleChildScrollView(
           child: Column(
@@ -138,7 +122,6 @@ class _HomePageState extends State<HomePage> {
                   padding: EdgeInsets.all(
                 5,
               )),
-
               Container(
                 decoration: BoxDecoration(
                   color: Colors.grey[200],
@@ -160,50 +143,45 @@ class _HomePageState extends State<HomePage> {
                 ),
                 width: 360,
                 height: 120,
-                child: Text('사용방법 : '),
+                child: Text(' ',style: TextStyle(fontWeight: FontWeight.bold),),
               ),
               SizedBox(
                 height: 20,
               ),
-
               TextField(
-
                 maxLength: 11,
                 controller: t1,
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.end,
                 inputFormatters: [ThousandsFormatter()],
-                decoration: InputDecoration(counterText: "",
+                decoration: InputDecoration(
+                  counterText: "",
                   hintText: ' 매 출 : 신용카드, 배달어플, 현금영수증',
-
-
                 ),
               ),
-              SizedBox(height: 10,
+              SizedBox(
+                height: 10,
               ),
-
               TextField(
-                maxLength:11,
+                maxLength: 11,
                 controller: t2,
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.end,
                 inputFormatters: [ThousandsFormatter()],
-                decoration: InputDecoration(counterText: "",
-                  hintText: '지출 1 :  농축산물,   식자재,   면세물품'
-                ),
-
+                decoration: InputDecoration(
+                    counterText: "", hintText: '지출 1 :  농축산물,   식자재,   면세물품'),
               ),
-              SizedBox(height: 10,),
-
+              SizedBox(
+                height: 10,
+              ),
               TextField(
                 maxLength: 11,
                 controller: t3,
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.end,
                 inputFormatters: [ThousandsFormatter()],
-                decoration: InputDecoration(counterText: "",
-                  hintText: '지출 2 : 공산품, 배달대행, 광고비,월세'
-                ),
+                decoration: InputDecoration(
+                    counterText: "", hintText: '지출 2 : 공산품, 배달대행, 광고비,월세'),
               ),
               SizedBox(
                 height: 20,
@@ -220,15 +198,14 @@ class _HomePageState extends State<HomePage> {
                 child: AnimatedContainer(
                     child: Icon(
                       Icons.currency_exchange_sharp,
-                      color: Colors.white,
+                      color: Colors.blue,
                     ),
-
                     height: 50,
                     width: 70,
                     duration: Duration(milliseconds: 1),
                     curve: Curves.easeInBack,
                     decoration: BoxDecoration(
-                        color: Colors.blueAccent,
+                        color: Colors.grey[200],
                         borderRadius: BorderRadius.circular(8),
                         boxShadow: click
                             ? null
@@ -247,11 +224,9 @@ class _HomePageState extends State<HomePage> {
                                 )
                               ])),
               ),
-
               SizedBox(
-                height: 50,
+                height: 15,
               ),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -274,18 +249,20 @@ class _HomePageState extends State<HomePage> {
                               spreadRadius: spredR)
                         ]),
                     child: Text(
-                      '매출 과세표준',
+                      '매출 과세표준',style: TextStyle(fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
                   SizedBox(
                     width: 50,
                   ),
-                  Container(  width: 110,
+                  Container(
+                      width: 125,
                       height: 30,
-
-
-                      child: Text(comma.format(taxincome),textAlign: TextAlign.end,)), //taxincome
+                      child: Text(
+                        comma.format(taxincome),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                        textAlign: TextAlign.end,
+                      )), //taxincome
                 ],
               ),
               SizedBox(
@@ -313,7 +290,7 @@ class _HomePageState extends State<HomePage> {
                               spreadRadius: spredR)
                         ]),
                     child: Text(
-                      '지출 과세표준',
+                      '지출 과세표준',style: TextStyle(fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
@@ -321,12 +298,12 @@ class _HomePageState extends State<HomePage> {
                     width: 50,
                   ),
                   Container(
-
-                      width: 110,
+                      width: 125,
                       height: 30,
-
-
-                      child: Text(comma.format(taxspending),textAlign: TextAlign.end,)),
+                      child: Text(
+                        comma.format(taxspending),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                        textAlign: TextAlign.end,
+                      )),
                 ],
               ),
               SizedBox(
@@ -354,7 +331,7 @@ class _HomePageState extends State<HomePage> {
                               spreadRadius: spredR)
                         ]),
                     child: Text(
-                      '신용카드 공제',
+                      '신용카드 공제',style: TextStyle(fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
@@ -362,11 +339,12 @@ class _HomePageState extends State<HomePage> {
                     width: 50,
                   ),
                   Container(
-                      width: 110,
+                      width: 125,
                       height: 30,
-
-
-                      child: Text(comma.format(creditcard_help),textAlign: TextAlign.end,)),
+                      child: Text(
+                        comma.format(creditcard_help),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                        textAlign: TextAlign.end,
+                      )),
                   //신용카드 공제 매출의 1.3%
                 ],
               ),
@@ -395,7 +373,7 @@ class _HomePageState extends State<HomePage> {
                               spreadRadius: spredR)
                         ]),
                     child: Text(
-                      '식자재매입공제',
+                      '식자재매입공제',style: TextStyle(fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
@@ -403,11 +381,12 @@ class _HomePageState extends State<HomePage> {
                     width: 50,
                   ),
                   Container(
-                      width: 110,
+                      width: 125,
                       height: 30,
-
-
-                      child: Text(comma.format(spending_Food_help),textAlign: TextAlign.end,)),
+                      child: Text(
+                        comma.format(spending_Food_help),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                        textAlign: TextAlign.end,
+                      )),
                   //의제매입공제 식자재 매출의 2억미만 8.26% , 2억 초과 7.24%
                 ],
               ),
@@ -425,7 +404,7 @@ class _HomePageState extends State<HomePage> {
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
-                              offset:  Offset(off2, off2),
+                              offset: Offset(off2, off2),
                               color: Colors.black38,
                               blurRadius: blurR,
                               spreadRadius: spredR),
@@ -436,7 +415,7 @@ class _HomePageState extends State<HomePage> {
                               spreadRadius: spredR)
                         ]),
                     child: Text(
-                      '예상 부가세',
+                      '예상 부가세',style: TextStyle(fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
@@ -444,24 +423,18 @@ class _HomePageState extends State<HomePage> {
                     width: 50,
                   ),
                   Container(
-                      width: 110,
+                      width: 125,
                       height: 30,
-
-
-                      child: Text(comma.format(estimate_tax),textAlign: TextAlign.end,)),
+                      child: Text(
+                        comma.format(estimate_tax),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                        textAlign: TextAlign.end,
+                      )),
                 ],
               ),
-
-              //제스쳐 디텍터, 애니매이션 컨테이너,
             ],
           ),
-        )
-            // (매출*1/10) - (공산품+서비스+광고비 지출 * 1/10) - 신용카드공제 - 의제매입공제
-
-            ),
+        )),
       ),
     );
   }
 }
-
-
